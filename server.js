a4bde16031436af4701bacf901606789a3378250
Javohir11011 <ajavohir545@example.com>
2025-01-11 15:06:44 +0500

    finished

--- server.js
+++ server.js
@@ -12,5 +12,5 @@ app.use('/test2', (req, res) => {
 })
 
 app.listen(1956, () => {
-  console.log("server jaaaa ajaajkdnsfjkaskfasbjasfkjasbfksjabfjasjkgbksjbgkasbjjgkasbgkasbgkasgaa 1956 port");
+  console.log("server jaaaa ajaajkdnsfjkaskfasbjasfkjasbfksjabfjasjkgbksjbgkaFDSGDGSDGFDSFGSDGSDLSKDGJKLSDFGKSDFGNSDGFSDFGDSGFsbjjgkasbgkasbgkasgaa 1956 port");
 });
